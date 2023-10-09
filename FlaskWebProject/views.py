cf1be13977eb9513081c97a4de5910c24292c1ca
Trung <88804707+trunglq205@users.noreply.github.com>
2023-10-09 22:35:08 +0700

    Add files via upload

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -70,7 +70,7 @@ def login():
             app.logger.warning("Invalid login attempt!")
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        app.logger.warning(f"admin logged in successfully")
+        app.logger.warning("admin logged in successfully")
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('home')
